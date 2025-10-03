1a1e9e8825e498ac126efb7f71db730451a8c9b7
Artem Ohanjanyan <artemo@wix.com>
2025-10-03 22:17:47 +0300

    Refactor P019.hs

--- P019.hs
+++ P019.hs
@@ -4,9 +4,16 @@ data DayOfWeek = Mon | Tue | Wed | Thu | Fri | Sat | Sun
 nextDayOfWeek Sun = Mon
 nextDayOfWeek day = succ day
 
+data Month
+  = Jan | Feb | Mar
+  | Apr | May | Jun
+  | Jul | Aug | Sep
+  | Oct | Nov | Dec
+  deriving (Show, Eq, Ord, Enum)
+
 data Day = Day
   { dYear :: Int
-  , dMonth :: Int
+  , dMonth :: Month
   , dDay :: Int
   , dDayOfWeek :: DayOfWeek
   }
@@ -16,24 +23,24 @@ isLeapYear y =
   y `mod` 4 == 0 && (y `mod` 100 /= 0 || y `mod` 400 == 0)
 
 isEndOfMonth day
-  | dDay day == 31 && any (== dMonth day) [1, 3, 5, 7, 8, 10, 12] = True
-  | dDay day == 30 && any (== dMonth day) [4, 6, 9, 11] = True
-  | dDay day == 28 && dMonth day == 2 && not (isLeapYear (dYear day)) = True
-  | dDay day == 29 && dMonth day == 2 && isLeapYear (dYear day) = True
+  | dDay day == 31 && any (== dMonth day) [Jan, Mar, May, Jul, Aug, Oct, Dec] = True
+  | dDay day == 30 && any (== dMonth day) [Apr, Jun, Sep, Nov] = True
+  | dDay day == 28 && dMonth day == Feb && not (isLeapYear (dYear day)) = True
+  | dDay day == 29 && dMonth day == Feb && isLeapYear (dYear day) = True
   | otherwise = False
 
-isEndOfYear day = dDay day == 31 && dMonth day == 12
+isEndOfYear day = dDay day == 31 && dMonth day == Dec
 
 nextDay day
   | isEndOfYear day = Day
     { dYear = dYear day + 1
-    , dMonth = 1
+    , dMonth = Jan
     , dDay = 1
     , dDayOfWeek = nextDayOfWeek (dDayOfWeek day)
     }
   | isEndOfMonth day = day
     { dDay = 1
-    , dMonth = dMonth day + 1
+    , dMonth = succ (dMonth day)
     , dDayOfWeek = nextDayOfWeek (dDayOfWeek day)
     }
   | otherwise = day
@@ -41,7 +48,7 @@ nextDay day
     , dDayOfWeek = nextDayOfWeek (dDayOfWeek day)
     }
 
-allDays = iterate nextDay (Day 1900 1 1 Mon)
+allDays = iterate nextDay (Day 1900 Jan 1 Mon)
 
 xxCenturyDays = takeWhile ((<= 2000) . dYear) $ dropWhile ((< 1901) . dYear) allDays
 
