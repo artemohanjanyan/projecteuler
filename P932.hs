132edd47161b17c6952487d034b7d5cda1e924e4
Artem Ohanjanyan <artemo@wix.com>
2025-11-16 12:32:30 +0200

    Speed up P932

--- P932.hs
+++ P932.hs
@@ -1,13 +1,18 @@
+digitN :: Int -> Int
+digitN n | n < 10 = 1
+digitN n = 1 + digitN (n `div` 10)
+
 is2025 :: Int -> Bool
 is2025 n | n < 10 = False
 is2025 n =
   let
-    nStr = show n
-    nL = length nStr
-    (p1, p2) = splitAt (nL `div` 2) nStr
-    n1 = read p1 :: Int
-    n2 = read p2 :: Int
-  in (n1 + n2) ^ 2 == n && (show n1 ++ show n2 == nStr)
+    nL = digitN n
+    f = 10 ^ ((nL + 1) `div` 2)
+    n1 = n `div` f
+    n2 = n `mod` f
+    n1L = digitN n1
+    n2L = digitN n2
+  in (n1 + n2) ^ 2 == n && (n1L + n2L == nL)
 
 t :: Int -> Int
 t n = sum $ filter is2025 $ map (^2) [1..(10^((n + 1) `div` 2) - 1)]
